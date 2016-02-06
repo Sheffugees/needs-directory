bc90d9765f01cbe514dc54d6eb8d6d43cf3df175
J-P Stacey <work@jpstacey.info>
2016-02-06 15:56:45 +0000

    Some templating done

--- src/Controllers/All.php
+++ src/Controllers/All.php
@@ -17,10 +17,68 @@ use Symfony\Component\HttpFoundation\Request;
 class All
 {
     /**
-     * Callback: index, trying to choose language.
+     * Page callback: welcome and pick language.
      */
     public function index(Request $request, Application $app)
     {
-        return $app['twig']->render('index.twig');
+        return $app['twig']->render('start.twig');
     }
+
+    /**
+     * Page callback: set location.
+     */
+    public function location(Request $request, Application $app, $lang)
+    {
+        if (!preg_match("/^[a-z]{2}\$/", $lang)) {
+            return $app->redirect('/');
+        }
+
+        return $app['twig']->render("location-$lang.twig");
+    }
+
+    /**
+     * Page callback: choose a need.
+     */
+    public function choose(Request $request, Application $app, $lang)
+    {
+        if (!preg_match("/^[a-z]{2}\$/", $lang)) {
+            return $app->redirect('/');
+        }
+
+        return $app['twig']->render("choose-$lang.twig");
+    }
+
+    /**
+     * Page callback: define a need.
+     */
+    public function need(Request $request, Application $app, $lang, $need)
+    {
+        if (!preg_match("/^[a-z]{2}\$/", $lang)) {
+            return $app->redirect('/');
+        }
+
+        if (!preg_match("/^[a-z]+\$/", $need)) {
+            return $app->redirect('/');
+        }
+
+
+        return $app['twig']->render("need-$lang-$need.twig");
+    }
+
+    /**
+     * Page callback: return some results.
+     */
+    public function results(Request $request, Application $app, $lang, $find)
+    {
+        if (!preg_match("/^[a-z]{2}\$/", $lang)) {
+            return $app->redirect('/');
+        }
+
+        if (!preg_match("/^[a-z]+\$/", $need)) {
+            return $app->redirect('/');
+        }
+
+        return $app['twig']->render("results-$lang-$find.twig");
+    }
+
 }
